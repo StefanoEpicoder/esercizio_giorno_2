403925827698a8d675aaec599e0294064c30bb47
Stefano <dantoni.stefano@libero.it>
2024-01-30 15:57:36 +0100

    Aggiungere i file di progetto.

---
+++ esercizio_giorno_2/Persona.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_giorno_2 //ESERCIZIO SENZA COSTRUTTORE
+{
+    internal class Persona// definisco la classe, in questo caso Persona
+    {
+        public string Nome { get; set; } // definisco le proprietà della classe Persona(sono un modo per accedere ai campi di una classe in modo controllato get e set
+        public string Cognome { get; set; } // definisco le proprietà della classe Persona(sono un modo per accedere ai campi di una classe in modo controllato get e set
+        public int Eta { get; set; } // definisco le proprietà della classe Persona(sono un modo per accedere ai campi di una classe in modo controllato get e set
+
+        public string GetNome() // scrivo i tre metodi di Nome, Cognome, Eta, sopra ho definito le proprità, qui ritorno il valore 
+        {
+            return Nome; // ritorno il valore
+        }
+        public string GetCognome() //metodo
+        { 
+            return Cognome; //ritorno il valore
+        }
+        public int GetEta() //metodo
+        { 
+            return Eta; //ritorno il valore
+        }
+        public string GetDettagli()
+        {
+            return "Nome: " + Nome + ", Cognome: " + Cognome + ", Eta: " + Eta;
+        }
+    }
+}
---
+++ esercizio_giorno_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_giorno_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona newPersona = new Persona// ho scelto di istanziare le proprietà nella classe Program dove stampo i dati dei metodi richiesti usando Console.writeLine-newPersona è il nuovo oggetto, il costruttore new mi creaq il nuovo oggetto della classe persona.
+            {
+                Nome = "Marco",
+                Cognome = "Pincopallo",
+                Eta = 99
+            };
+
+            Console.WriteLine(newPersona.GetNome());
+            Console.WriteLine(newPersona.GetCognome());
+            Console.WriteLine(newPersona.GetEta());
+            Console.WriteLine("I dettagli della persona sono: \n" + newPersona.GetDettagli());
+            Console.ReadLine();
+        }
+
+    }
+}
